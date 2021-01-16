ee7aee5af33a01caed9dce8238065f71821584f4
Bartosz Król <badoszk@gmail.com>
2021-01-16 17:27:35 +0100

    update endpoints

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ const {get} = require('snekfetch')
 const endpoints = require('./endpoints.json')
 
 class ApiClient {
-  constructor(token) {
-    const self = this
-    this.token = token
-    this.endpoints = {}
-    this.baseURL = 'https://api.badosz.com'
-    Object.keys(endpoints.sfw).forEach(async endpoint => {
-      this.endpoints[endpoint] = async function(params = {}) {
-        const response = await get(`${self.baseURL}${endpoints.sfw[endpoint]}`)
-        .set({
-              Authorization: self.token
-             })
-        .query(params)
-        return response.body
-      }
-    })
+    constructor(token) {
+        const self = this
+        this.token = token
+        this.endpoints = {}
+        this.baseURL = 'https://api.badosz.com'
+        Object.keys(endpoints).forEach(async endpoint => {
+            this.endpoints[endpoint] = async function(params = {}) {
+                const response = await get(`${self.baseURL}${endpoints[endpoint]}`).set({
+                    Authorization: self.token
+                }).query(params)
+                return response.body
+            }
+        })
   }
 }
 
